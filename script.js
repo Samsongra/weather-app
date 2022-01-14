3f4c53906aa70d75f54a4ee1749a2d04c9600a4f
Samsongra <93117454+Samsongra@users.noreply.github.com>
2022-01-14 15:44:42 +0100

    stworzenie pliku script.js i dodanie kodu do api

---
+++ script.js
@@ -0,0 +1,9 @@
+let weather = {
+  apiKey: "API KEY GOES HERE",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
