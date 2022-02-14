f2b2d7cab90ebdc0440fa20f68f085fd9874d627
Borys Gaik <93117711+ShamDumb@users.noreply.github.com>
2022-02-14 22:14:35 +0100

    Green
    Napisany i działa

--- search_cities.js
+++ search_cities.js
@@ -1,6 +1,39 @@
 const getCities = require("./cities");
 
-function searchCountries() {
-  return Error("Not implemented error");
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
+function searchCountries(search = null) {
+  let toSearch = [];
+
+  var arr = getCities().split("\n");
+
+  for (var i = 1; i < arr.length; i++) {
+    const line = arr[i];
+    const line_array = line.split(",");
+    toSearch.push(line_array[1]);
+  }
+  var unique = toSearch.filter(onlyUnique);
+
+  if (search == null) {
+    let toReturn = [];
+    unique.map((e) => {
+      if (e.toString().toLowerCase().startsWith("a")) {
+        toReturn.push(e);
+      }
+    });
+    return toReturn;
+  } else {
+    let toReturn = [];
+    unique.map((e) => {
+      if (
+        e.toString().toLowerCase().startsWith(search.toString().toLowerCase())
+      ) {
+        toReturn.push(e);
+      }
+    });
+    return toReturn;
+  }
 }
 module.exports = searchCountries;
--- testy.test.js
+++ testy.test.js
@@ -1,13 +1,14 @@
 const searchCountries = require("./search_cities");
 
-test("if no argument is given, return cities beginning with the letter A", () => {
-  expect(searchCountries().length).toBe(5);
+test("if no argument is given, return countries beginning with the letter A", () => {
+  expect(searchCountries().length).toBe(15);
 });
 
-test("when you enter Poland, it returns cities from Poland", () => {
-  expect(searchCountries("Poland").length).toBe(5);
+test("when you enter Poland, it should return 1 result", () => {
+  expect(searchCountries("Poland").length).toBe(1);
 });
 
-test("after entering Sw/Sz it should return 2 results", () => {
-  expect(searchCountries("Sw/Sz").length).toBe(2);
+test("after entering Sw it should return 3 results", () => {
+  expect(searchCountries("Sw").length).toBe(3);
+  //Switzerland, Sweden, Swaziland
 });
