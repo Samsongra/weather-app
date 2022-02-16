918679654048e2ae06f4818a92b15188b6d64a01
arx225 <77251253+arx225@users.noreply.github.com>
2022-02-16 17:35:33 +0100

    ez

--- search.js
+++ search.js
@@ -1,5 +1,20 @@
-function validateCity() {
-  return new Error("jeszcze nie zostało napisane :c");
+function validateCity(nazwa = null) {
+  if (nazwa == null) {
+    return false;
+  }
+  if (nazwa == "") {
+    return false;
+  }
+  if (/^\d+$/.test(nazwa)) {
+    return false;
+  }
+  if (!/^[a-zA-Z0-9. -_?]*$/.test(nazwa)) {
+    return false;
+  }
+  if (nazwa.startsWith(" ")) {
+    return false;
+  }
+  return true;
 }
 
 module.exports = validateCity;
